f972fe2193e142ce45c49c69efb89665272c439f
Waldemar Galizzi <86132080+Wallesi@users.noreply.github.com>
2023-02-05 21:02:34 -0300

    Comitiendo auth sin la /

--- src/main/java/com/portfolio/yoProgramo/security/controller/AuthController.java
+++ src/main/java/com/portfolio/yoProgramo/security/controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("auth")
 @CrossOrigin(origins={"https://frontwaldemar.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
